0e4597361d2804d8415bb8b476cadeb6710eafbb
Kevin Fairclough <kevin.fairclough@gmail.com>
2023-05-17 16:54:02 +0100

    Add assign to type EvaluatorFunc

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -13,13 +13,14 @@ interface ParserOptions {
 type EvaluatorFunc = {
   (scope?: any, context?: any): any;
   ast: any;
+  assign: (scope: any, value: any) => any;
 };
 
 type CompileFunc = {
-    ( tag: string, lexerOptions?: LexerOptions) : EvaluatorFunc;
-    cache: {
-        [x:string]: any
-    }
+  (tag: string, lexerOptions?: LexerOptions): EvaluatorFunc;
+  cache: {
+    [x: string]: any;
+  };
 };
 
 export const compile: CompileFunc;
@@ -28,9 +29,9 @@ export class Lexer {
   constructor(options?: LexerOptions);
 }
 
-export const filters : {
-    [x :string]: (input: any, ...args: any[]) => any;
-}
+export const filters: {
+  [x: string]: (input: any, ...args: any[]) => any;
+};
 
 export class Parser {
   constructor(options?: ParserOptions);
