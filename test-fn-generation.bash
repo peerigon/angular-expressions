f4160584039a50f35a6e75b1dc5a6834a5443a9c
Edgar HIPP <hipp.edg@gmail.com>
2026-04-27 15:01:06 +0200

    [tmp] OK

--- test-fn-generation.bash
+++ test-fn-generation.bash
@@ -4,23 +4,23 @@ set -euo pipefail
 export PATH="./node_modules/.bin/:$PATH"
 
 node ./node_modules/.bin/mocha test/main.test.js || true
-prettier /tmp/code.js >gen-code-eslint/code.js
-sed -i '$d' gen-code-eslint/code.js
-sed -i '1d' gen-code-eslint/code.js
+prettier /tmp/code.js >gen-code/code.js
+sed -i '$d' gen-code/code.js
+sed -i '1d' gen-code/code.js
 
-c="$(cat gen-code-eslint/code.js)"
+c="$(cat gen-code/code.js)"
 echo "function plus(a,b) { return a + b }
-$c"  >gen-code-eslint/code.js
+$c"  >gen-code/code.js
 
 code=0
-tsc gen-code-eslint/code.js --allowJs --checkJs --noEmit --noImplicitAny false || code="$?"
+tsc gen-code/code.js --allowJs --checkJs --noEmit --noImplicitAny false || code="$?"
 if [ "$code" != "0" ]; then
     echo "Typescript did not validate this file"
-    cat gen-code-eslint/code.js
+    cat gen-code/code.js
     exit "$code"
 fi
 echo "Typescript ok !"
 
-cd gen-code-eslint
+cd gen-code
 ../node_modules/.bin/eslint --config eslint.config.mjs code.js
 echo "Eslint ok"
